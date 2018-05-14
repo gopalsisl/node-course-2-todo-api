93d211396b8605d318ce62b710c32b8512ece920
gopal_kumar <gopal_kumar@1234>
2018-05-14 18:44:41 +0530

    code update by gopal

--- server/server.js
+++ server/server.js
@@ -20,8 +20,17 @@ app.post('/todos',(req,res)=>{
     })
 
 })
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.status(200).send({todos});
+    },(err)=>{
+        res.status(400).send(err); 
+    })
+})
 
 
 app.listen('3000',()=>{
     console.log('App started at port 3000');
 })
+
+module.exports={app}
